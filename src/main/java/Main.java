5b5d6ffd6b072ba94c5ac06b5ca66325a0e5f086
amaro <amaro@DESKTOP-ANRLK55>
2021-03-21 02:01:55 -0500

    Lab 6 project files

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("Susan Meyers", 47899, "Accounting", "Vice President");
+        Employee emp2 = new Employee("Mark Jones", 39119, "IT", "Programmer");
+        Employee emp3 = new Employee("Joy Rogers", 81774, "Manufacturing", "Engineer");
+        
+        System.out.println(emp1);
+        System.out.println(emp2);
+        System.out.println(emp3);
+    }
+    
+}
